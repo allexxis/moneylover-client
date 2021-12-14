8d89ad23c9f7b13dadd80cae61f0b8f4e10faf8e
Alexis Valenciano <gabr.933@gmail.com>
2021-12-14 14:36:26 -0600

    Added logout

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ const income = require('./commands/income');
 const expense = require('./commands/expense');
 const moneylover = require('./moneylover');
 const wallets = require('./commands/wallets');
+const logout = require('./commands/logout');
 
-module.exports = { login, config, income, moneylover, expense, wallets };
+module.exports = {
+   login,
+   config,
+   income,
+   moneylover,
+   expense,
+   wallets,
+   logout,
+};
